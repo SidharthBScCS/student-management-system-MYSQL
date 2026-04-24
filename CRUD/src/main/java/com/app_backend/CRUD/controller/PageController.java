e424c1c53459efee86f32ed804d933134de06242
SIDHARTH <codefather0003@gmail.com>
2026-04-24 20:40:06 +0530

    List all the students in web page

--- CRUD/src/main/java/com/app_backend/CRUD/controller/PageController.java
+++ CRUD/src/main/java/com/app_backend/CRUD/controller/PageController.java
@@ -1,13 +1,22 @@
 package com.app_backend.CRUD.controller;
 
+import com.app_backend.CRUD.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
+    private final StudentService studentService;
+
+    public PageController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("students", studentService.readStudents());
         return "index";
     }
 }
