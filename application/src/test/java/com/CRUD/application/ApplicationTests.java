b6fd9d905285941c30eb26a5e1325eccce4f69e5
SIDHARTH <codefather0003@gmail.com>
2026-04-24 15:30:44 +0530

    Springboot folder structure initialized

---
+++ application/src/test/java/com/CRUD/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUD.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
